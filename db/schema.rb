d191405ef30d643d8b16f8f32515a50999d6956f
c1017067@planet.kanazawa-it.ac.jp <okaka7wnf@i.softbank.jp>
2021-07-12 20:01:57 +0900

    controllとmodelを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_07_12_102745) do
+
+  create_table "articles", charset: "utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
